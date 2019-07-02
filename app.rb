c87d55c7bed6f07f6913b440ae4ab9d82a88ce1c
Student <student@MAKERSs-MacBook-Pro-9.local>
2019-07-02 16:18:03 +0100

    sign_in page, Ajax not working

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
 require 'sinatra/base'
+require 'json'
 
 class MakersBnB < Sinatra::Base
   enable :sessions
 
-  get '/' do 
-    'hello world'
+  get '/' do
+    erb :index
   end 
+
+  get '/sign_in' do
+    erb :sign_in
+  end
+
+  get '/makersbnb' do
+    erb :home
+  end
 
   run! if app_file == $0
 end
--- spec/backend/features/sign_up_spec.rb
+++ spec/backend/features/sign_up_spec.rb
@@ -4,6 +4,26 @@ RSpec.feature 'Sign in' do
 
   scenario 'it displays hello world' do
     visit '/'
-    expect(page).to have_text('hello world')
+    expect(page).to have_text('Makers BnB')
+  end
+
+  scenario 'it goes to a sign in page' do
+    visit '/'
+    click_button 'sign_in'
+    expect(current_path).to eq('/sign_in')
+    expect(page).to have_text('Makers BnB')
+  end
+
+  scenario 'it record the name and display it in the home page' do 
+    visit '/'
+    click_button 'sign_in'
+    expect(current_path).to eq('/sign_in')
+    fill_in 'name', with: 'user'
+    # fill_in 'email', with: 'email'
+    # fill_in 'password', with: 'password'
+
+    click_button 'submit'
+    expect(current_path).to eq('/makersbnb')
+    expect(page).to have_content('Makers BnB', 'user')
   end
 end
